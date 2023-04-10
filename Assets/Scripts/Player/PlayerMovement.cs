9849767e6f9175a46a5bb0c9eb199f2ab50a5016
Jonas Ingelstad <JonasIngelstad@Gmail.com>
2023-04-10 23:41:26 +0200

    Implemented gyro controls

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -8,21 +9,50 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private VariableJoystick joystick;
 
     [SerializeField] private Vector2 moveInput;
+    public bool useGrav = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         joystick = GameObject.Find("Joystick").GetComponent<VariableJoystick>();
         moveInput = new Vector2();
+
+        EnableGrav();
     }
 
     private void Update()
     {
-        moveInput = new Vector2(joystick.Horizontal, joystick.Vertical);
+        if (!useGrav)
+            moveInput = new Vector2(joystick.Horizontal, joystick.Vertical);
+        else
+        {
+            moveInput = GravitySensor.current.gravity.ReadValue();
+        }
+    }
+
+    public void EnableGrav()
+    {
+        if (useGrav) return;
+
+        if (SystemInfo.supportsGyroscope)
+        {
+            InputSystem.EnableDevice(GravitySensor.current);
+            joystick.gameObject.SetActive(false);
+            useGrav = true;
+        }
+    }
+
+    public void DisableGrav()
+    {
+        if (!useGrav) return;
+
+        InputSystem.DisableDevice(GravitySensor.current);
+        joystick.gameObject.SetActive(true);
+        useGrav = false;
     }
 
     public void DoMove(float speed)
     {
-        rb.velocity = moveInput * speed;
+        rb.velocity = moveInput * speed * 5;
     }
 }
